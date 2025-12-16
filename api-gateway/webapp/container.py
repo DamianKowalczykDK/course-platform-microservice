3d9e323a2ae9d0e36f30582bb5d7d8b40f9310de
Damian <damiankowalczyk3688@gmail.com>
2025-12-16 17:26:23 +0100

    user_service=providers.Singleton(UserService)

--- api-gateway/webapp/container.py
+++ api-gateway/webapp/container.py
@@ -1,5 +1,6 @@
 from dependency_injector import providers, containers
 from webapp.services.auth.services import AuthService
+from webapp.services.users.services import UserService
 
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(
@@ -8,4 +9,5 @@ class Container(containers.DeclarativeContainer):
         ]
     )
     auth_service=providers.Singleton(AuthService)
+    user_service=providers.Singleton(UserService)
 
