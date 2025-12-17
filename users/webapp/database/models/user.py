6c1dac3073c27856d9903c8242f805d040e70785
Damian <damiankowalczyk3688@gmail.com>
2025-12-18 00:21:53 +0100

    refactor(users-webapp): update user model and user repository

--- users/webapp/database/models/user.py
+++ users/webapp/database/models/user.py
@@ -1,7 +1,9 @@
+import uuid
 from datetime import datetime, timedelta, timezone
 from enum import Enum as PyEnum
 from typing import Literal
 
+from werkzeug.security import generate_password_hash
 from mongoengine import Document, StringField, BooleanField, DateTimeField, EmailField, EnumField
 
 class GenderType(PyEnum):
@@ -19,7 +21,54 @@ class User(Document):
     password_hash: str = StringField(required=True)
     role: Literal["user", "admin"] = StringField(default="user", choices=["user", "admin"])
     is_active: bool = BooleanField(default=False)
+
     activation_code: str = StringField(required=True, unique=True)
     activation_created_at: datetime = DateTimeField(default=lambda: datetime.now(timezone.utc))
+
+    reset_password_token: str | None = StringField(unique=True, sparse=True, default=None)
+    reset_password_expires_at: datetime | None = DateTimeField(default=None)
+
     created_at: datetime = DateTimeField(default=lambda: datetime.now(timezone.utc))
     updated_at: datetime = DateTimeField(default=lambda: datetime.now(timezone.utc))
+
+    def activate(self) -> None:
+        self.is_active = True
+        self.activation_code = str(self.id)
+        self.updated_at = datetime.now(timezone.utc)
+
+    def update_activation_code(self) -> None:
+        self.activation_code = str(uuid.uuid4())
+        self.activation_created_at = datetime.now(timezone.utc)
+        self.updated_at = datetime.now(timezone.utc)
+
+    def set_reset_password_token(self, expires_minutes: int = 15) -> None:
+        self.reset_password_token = str(uuid.uuid4())
+        self.reset_password_expires_at = datetime.now(timezone.utc) + timedelta(minutes=expires_minutes)
+        self.updated_at = datetime.now(timezone.utc)
+
+    def clear_reset_password_token(self) -> None:
+        self.reset_password_token = None
+        self.reset_password_expires_at = None
+        self.updated_at = datetime.now(timezone.utc)
+
+    def reset_password(self, new_password: str) -> None:
+        self.password_hash = generate_password_hash(new_password)
+        self.clear_reset_password_token()
+
+    def is_token_rest_password_valid(self, token: str) -> bool:
+        if self.reset_password_token != token:
+            return False
+
+        if not self.reset_password_expires_at:
+            return False
+
+        now_utc = datetime.now(timezone.utc)
+
+        created_at = self.reset_password_expires_at
+        if created_at.tzinfo is None:
+            created_at = created_at.replace(tzinfo=timezone.utc)
+
+        return created_at > now_utc
+
+
+
--- users/webapp/database/repositories/user.py
+++ users/webapp/database/repositories/user.py
@@ -1,11 +1,16 @@
 import uuid
-from datetime import timezone, datetime
+from datetime import timezone, datetime, timedelta
 
 from webapp.database.models.user import User
 from mongoengine.queryset.visitor import Q
 
 class UserRepository:
     def create_user(self, user: User) -> User:
+        user.save()
+        return user
+
+    def save(self, user: User) -> User:
+        user.updated_at = datetime.now(timezone.utc)
         user.save()
         return user
 
@@ -27,22 +32,8 @@ class UserRepository:
     def get_by_activation_code(self, activation_code: str) -> User | None:
         return User.objects(activation_code=activation_code).first()
 
-    def get_all(self) -> list[User]:
-        return list(User.objects.all())
-
-    def activate(self, user: User) -> None:
-        user.is_active = True
-        user.activation_code = ""
-        user.updated_at = datetime.now(timezone.utc)
-        user.save()
-
-    def update_activation_code(self, user: User) -> User:
-        user.activation_code = str(uuid.uuid4())
-        now_utc = datetime.now(timezone.utc)
-        user.activation_created_at = now_utc
-        user.updated_at = datetime.now(timezone.utc)
-        user.save()
-        return user
+    def get_by_reset_password_token(self, token: str) -> User | None:
+        return User.objects(reset_password_token=token).first()
 
 
 
