4cbb8fa65d9edde0bca51d0ab96b722120cb2d23
Damian <damiankowalczyk3688@gmail.com>
2025-12-19 00:00:02 +0100

    add tests for user API flow and User model

---
+++ users/tests/test_api_user_flow.py
@@ -0,0 +1,109 @@
+
+from urllib.parse import urlparse
+from unittest.mock import patch, MagicMock
+from flask.testing import FlaskClient
+from flask import Flask
+from testcontainers.mongodb import MongoDbContainer
+from typing import Generator
+
+from webapp import create_app
+from webapp.database.models.user import User
+from webapp.settings import Config
+import pytest
+
+@pytest.fixture(scope="module")
+def mongo_db_container_url() -> Generator[str, None, None]:
+    with MongoDbContainer("mongo:latest") as mongo:
+        yield mongo.get_connection_url()
+
+@pytest.fixture
+def app(mongo_db_container_url: str) -> Generator[Flask, None, None]:
+    parsed = urlparse(mongo_db_container_url)
+
+    Config.MONGODB_DB = parsed.path.lstrip("/")
+    Config.MONGODB_HOST = parsed.hostname or "localhost"
+    Config.MONGODB_PORT = parsed.port or 27017
+    if parsed.username:
+        Config.MONGODB_USERNAME = parsed.username
+
+    if parsed.password:
+        Config.MONGODB_PASSWORD = parsed.password
+
+    app: Flask = create_app()
+    app.config.update(
+        TESTING=True,
+        USER_ACTIVATION_EXPIRATION_MINUTES=5,
+        RESET_PASSWORD_EXPIRATION_MINUTES=5
+    )
+
+    yield app
+    User.drop_collection()
+
+@pytest.fixture
+def client(app: Flask) -> FlaskClient:
+    return app.test_client()
+
+@patch("webapp.services.email_service.EmailService.send_email")
+def test_user_flow(mock_email: MagicMock, client: FlaskClient) -> None:
+        resp = client.post("/api/users/",
+            json={
+                "username": "Jon30",
+                "first_name": "Jon",
+                "last_name": "Doe",
+                "email": "jon@example.com",
+                "password": "secret123",
+                "password_confirmation": "secret123",
+                "gender": "Male",
+                "role": "user",
+            }
+            )
+
+        assert resp.status_code == 201
+        data = resp.get_json()
+        assert data["is_active"] is False
+
+
+        resp = client.get("/api/users/resend-activation/Jon30")
+        assert resp.status_code == 200
+
+        user = User.objects.get(username="Jon30")
+        activation_code = user.activation_code
+
+
+        resp = client.patch("/api/users/activate", json={"code": activation_code})
+        assert resp.status_code == 200
+        data = resp.get_json()
+        assert data["is_active"] is True
+
+        resp = client.get("/api/users/Jon30")
+        assert resp.status_code == 200
+
+
+        resp = client.post("/api/users/login", json={"identifier": "Jon30", "password": "secret123"})
+        assert resp.status_code == 200
+
+
+        resp = client.post("/api/users/forgot-password", json={"identifier": "Jon30"})
+        assert resp.status_code == 200
+        data = resp.get_json()
+        assert data["message"] == "If the email exist, a reset link has been sent."
+
+        user.reload()
+        reset_token = user.reset_password_token
+        assert reset_token is not None
+
+        resp = client.post("/api/users/reset-password",
+               json={
+                   "token": reset_token,
+                   "new_password": "newpassword123"
+               }
+               )
+        assert resp.status_code == 200
+        data = resp.get_json()
+        assert data["message"] == "Password has been reset successfully."
+
+
+
+
+
+
---
+++ users/tests/test_user_model.py
@@ -0,0 +1,75 @@
+import uuid
+from datetime import datetime, timezone, timedelta
+
+import pytest
+
+from webapp.database.models.user import User
+
+
+@pytest.fixture
+def user() -> User:
+    return User(
+        username="test",
+        first_name="Jon",
+        last_name="Doe",
+        email="test@example.com",
+        password_hash="hash1234",
+        gender="Male",
+        activation_code="code12345"
+    )
+
+def test_activate_sets_active_and_changes_code(user: User) -> None:
+    user.activation_code = user.activation_code
+    user.activate()
+    assert user.is_active is True
+    assert user.activation_code == str(user.id)
+
+def test_update_activation_code_changes_code_and_timestamp(user: User) -> None:
+    old_activation_code = user.activation_code
+    user.update_activation_code()
+    assert user.activation_code != old_activation_code
+    assert user.activation_created_at < datetime.now(timezone.utc)
+
+def test_set_and_clear_reset_password_token(user: User) -> None:
+    user.set_reset_password_token(expires_minutes=1)
+    assert user.reset_password_token is not None
+    assert user.reset_password_expires_at is not None
+    assert user.reset_password_expires_at > datetime.now(timezone.utc)
+
+    user.clear_reset_password_token()
+    assert user.reset_password_token is None
+    assert user.reset_password_expires_at is None
+
+def test_reset_password(user: User) -> None:
+    user.set_reset_password_token(expires_minutes=1)
+    old_password_hash = user.password_hash
+    user.reset_password("newpassword123")
+    assert user.password_hash != old_password_hash
+
+def test_is_token_reset_password_valid(user: User) -> None:
+    user.set_reset_password_token(expires_minutes=1)
+    token = user.reset_password_token
+
+    assert token is not None
+    assert user.is_token_rest_password_valid(token) is True
+
+def test_if_token_reset_password_valid_returns_false(user: User) -> None:
+    user.set_reset_password_token(expires_minutes=1)
+    assert user.is_token_rest_password_valid("bad_token") is False
+
+
+def test_if_token_reset_password_not_expires_at(user: User) -> None:
+    user.set_reset_password_token()
+    token = user.reset_password_token
+    user.reset_password_expires_at = None
+
+    assert token is not None
+    assert user.is_token_rest_password_valid(token) is False
+
+def test_if_token_reset_password_valid_date_time(user: User) -> None:
+    user.reset_password_token = "token-test"
+    user.reset_password_expires_at = datetime.now()
+    assert user.is_token_rest_password_valid("token-test") is True
+
+
+
