8816d978f725095f3562d8827bc5fbeb3d94dc49
Damian <damiankowalczyk3688@gmail.com>
2025-12-27 11:59:39 +0100

    refactor(api_gateway-webapp): refactor code

--- api-gateway/webapp/api/__init__.py
+++ api-gateway/webapp/api/__init__.py
@@ -3,13 +3,10 @@ from flask import Blueprint
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 from .auth import auth_bp
-
 api_bp.register_blueprint(auth_bp)
 
 from .users import users_bp
-
 api_bp.register_blueprint(users_bp)
 
 from .protected import protected_bp
-
 api_bp.register_blueprint(protected_bp)
