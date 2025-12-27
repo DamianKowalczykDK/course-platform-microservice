632a064a92e5499ffd491bbfca3751ff78906b70
Damian <damiankowalczyk3688@gmail.com>
2025-12-27 12:25:49 +0100

    feat(api-gateway-webapp): add errorhandler handle_api_exception and remove logger

--- api-gateway/webapp/api/error_handlers.py
+++ api-gateway/webapp/api/error_handlers.py
@@ -1,13 +1,30 @@
+import traceback
+from typing import TypedDict
 from flask import Flask, jsonify
 from flask.typing import ResponseReturnValue
+from webapp.services.exceptions import ApiException
+
 
 def register_error_handlers(app: Flask) -> None:
+    @app.errorhandler(ApiException)
+    def handle_api_exception(error: ApiException) -> ResponseReturnValue:
+        return jsonify({
+            "message": error.message,
+            "status": error.error_code,
+        }), error.status_code
+
     @app.errorhandler(404)
     def handle_not_found_error(error: Exception) -> ResponseReturnValue:
-        app.logger.exception(f"Handle error: {str(error)}")
-        return jsonify({f"Message": "Not Found"}), 404
+        return jsonify({
+            "Message": "The requested resource could not be found.",
+            "error": "not_found",
+           }
+        ), 404
 
     @app.errorhandler(Exception)
-    def handle_error(error: Exception) -> ResponseReturnValue:
-        app.logger.exception(f"Handle error: {str(error)}")
-        return jsonify(f"Message: {str(error)}"), 500
+    def handle_generic(error: Exception) -> ResponseReturnValue:
+        return jsonify({
+            "Message": "Unexpected error.",
+            "error": "internal_error"
+        }), 500
+
