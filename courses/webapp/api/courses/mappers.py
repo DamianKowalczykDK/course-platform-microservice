015932ee1998d123f30a922ef4a4b6fb8377b194
Damian <damiankowalczyk3688@gmail.com>
2026-02-02 01:30:40 +0100

    feat(courses-webapp): add schemas and mappers for API

---
+++ courses/webapp/api/courses/mappers.py
@@ -0,0 +1,38 @@
+from webapp.api.courses.schemas import CreateCourseSchema, CourseResponseSchema, CourseIdSchema, CourseNameSchema, \
+    UpdateCourseSchema
+from webapp.services.courses.dtos import CreateCourseDTO, ReadCourseDTO, CourseIdDTO, CourseNameDTO, UpdateCourseDTO
+
+
+def to_dto_create(schema: CreateCourseSchema) -> CreateCourseDTO:
+    return CreateCourseDTO(
+        name=schema.name,
+        description=schema.description,
+        max_participants=schema.max_participants,
+        start_date=schema.start_date,
+        end_date=schema.end_date,
+    )
+
+def to_schema_course(dto: ReadCourseDTO) -> CourseResponseSchema:
+    return CourseResponseSchema(
+        id=dto.id,
+        name=dto.name,
+        description=dto.description,
+        max_participants=dto.max_participants,
+        start_date=dto.start_date,
+        end_date=dto.end_date
+    )
+
+def to_dto_course_id(schema: CourseIdSchema) -> CourseIdDTO:
+    return CourseIdDTO(course_id=schema.course_id)
+
+def to_dto_course_name(schema: CourseNameSchema) -> CourseNameDTO:
+    return CourseNameDTO(name=schema.name)
+
+def to_dto_update_course(schema: UpdateCourseSchema) -> UpdateCourseDTO:
+    return UpdateCourseDTO(
+        name=schema.name,
+        description=schema.description,
+        max_participants=schema.max_participants,
+        start_date=schema.start_date,
+        end_date=schema.end_date,
+    )
---
+++ courses/webapp/api/courses/schemas.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+class CreateCourseSchema(BaseModel):
+    name: str =  Field(min_length=2, max_length=64)
+    description: str = Field(max_length=255)
+    start_date: datetime
+    end_date: datetime
+    max_participants: int | None = Field(None, ge=0)
+
+
+class CourseResponseSchema(BaseModel):
+    id: int
+    name: str
+    description: str
+    max_participants: int | None
+    start_date: datetime
+    end_date: datetime
+
+class CourseIdSchema(BaseModel):
+    course_id: int
+
+class CourseNameSchema(BaseModel):
+    name: str = Field(min_length=3, max_length=64)
+
+class UpdateCourseSchema(BaseModel):
+    name: str =  Field(min_length=2, max_length=64)
+    description: str = Field(max_length=255)
+    start_date: datetime
+    end_date: datetime
+    max_participants: int | None = Field(None, ge=0)
+
+
