2a641ae52fa93fcc6b559cf7b51b565fbe9ff03a
Damian <damiankowalczyk3688@gmail.com>
2026-02-05 22:01:39 +0100

    feat(enrolments-webapp) add flask mail

--- enrolments/webapp/__init__.py
+++ enrolments/webapp/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, current_app
 from .settings import config
-from .extensions import db,  migrate
+from .extensions import db,  migrate, mail
 from .container import Container
-# from .api import api_bp
+from .api import api_bp
 from .api.error_handlers import register_error_handlers
 
 def create_app() -> Flask: # pragma: no cover
@@ -11,13 +11,14 @@ def create_app() -> Flask: # pragma: no cover
     config['default'].init_app(app)
 
     db.init_app(app)
+    mail.init_app(app)
     migrate.init_app(app, db)
 
     container = Container()
     container.wire()
 
     register_error_handlers(app)
-    # app.register_blueprint(api_bp)
+    app.register_blueprint(api_bp)
 
     with app.app_context():
         app.logger.info("[ENROLMENTS ROUTES]")
