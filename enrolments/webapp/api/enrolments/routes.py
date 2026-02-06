49214554c9a6ce7684d75f21e6e5c587556974fb
Damian <damiankowalczyk3688@gmail.com>
2026-02-06 15:26:39 +0100

    feat(enrolments-webapp) add routes for API

--- enrolments/webapp/api/enrolments/routes.py
+++ enrolments/webapp/api/enrolments/routes.py
@@ -12,7 +12,7 @@ from . import enrolment_bp
 def create_enrolment(enrolment_service: EnrolmentService = Provide[Container.enrolment_service]) -> ResponseReturnValue:
     payload = CreateEnrolmentSchema.model_validate(request.get_json() or {})
     dto = to_create_enrolment_dto(payload)
-    read_dto = enrolment_service.create_course_for_user(dto)
+    read_dto = enrolment_service.create_enrolment_for_user(dto)
     return jsonify(to_enrolment_response_schema(read_dto).model_dump(mode="json")), 201
 
 @enrolment_bp.patch("/paid")
@@ -21,4 +21,12 @@ def set_paid(enrolment_service: EnrolmentService = Provide[Container.enrolment_s
     payload = EnrolmentIdSchema.model_validate(request.get_json() or {})
     dto = to_enrolment_id_dto(payload)
     read_dto = enrolment_service.set_paid(dto)
+    return jsonify(to_enrolment_response_schema(read_dto).model_dump(mode="json")), 200
+
+@enrolment_bp.get("/<int:enrolment_id>")
+@inject
+def get_by_id(enrolment_id: int, enrolment_service: EnrolmentService= Provide[Container.enrolment_service]) -> ResponseReturnValue:
+    payload = EnrolmentIdSchema.model_validate({"enrolment_id": enrolment_id})
+    dto =  to_enrolment_id_dto(payload)
+    read_dto = enrolment_service.get_by_id(dto)
     return jsonify(to_enrolment_response_schema(read_dto).model_dump(mode="json")), 200
