2074550f096f5dd970497c758b546e6264575c7d
Damian <damiankowalczyk3688@gmail.com>
2026-02-08 18:59:49 +0100

    feat(enrolments-webapp) add method get_active for repository

--- enrolments/webapp/database/repositories/enrolments.py
+++ enrolments/webapp/database/repositories/enrolments.py
@@ -1,5 +1,5 @@
 from sqlalchemy import select, and_
-from webapp.database.models.enrolments import Enrolment
+from webapp.database.models.enrolments import Enrolment, Status
 from webapp.database.repositories.generic import GenericRepository
 from webapp.extensions import db
 
@@ -7,7 +7,7 @@ class EnrolmentRepository(GenericRepository[Enrolment]):
     def __init__(self) -> None:
         super().__init__(Enrolment)
 
-    def get_by_user_and_course(self, course_id: int, user_id: int) -> Enrolment | None:
+    def get_by_user_and_course(self, course_id: int, user_id: str) -> Enrolment | None:
         stmt = select(Enrolment).where(and_(Enrolment.course_id == course_id, Enrolment.user_id == user_id))
         return db.session.scalars(stmt).first()
 
@@ -15,3 +15,6 @@ class EnrolmentRepository(GenericRepository[Enrolment]):
         stmt = select(Enrolment).where(Enrolment.id == enrolment_id)
         return db.session.scalars(stmt).first()
 
+    def get_active(self) -> list[Enrolment]:
+        stmt = select(Enrolment).where(Enrolment.status == Status.ACTIVE)
+        return list(db.session.scalars(stmt).all())
