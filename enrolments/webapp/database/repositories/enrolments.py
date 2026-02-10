f0a32554c77b0d358a2959190ad394c0d7598e63
Damian <damiankowalczyk3688@gmail.com>
2026-02-10 13:30:15 +0100

    feat(enrolments-webapp): add mark_expired_enrolments_completed method for repository

--- enrolments/webapp/database/repositories/enrolments.py
+++ enrolments/webapp/database/repositories/enrolments.py
@@ -1,7 +1,11 @@
-from sqlalchemy import select, and_
+from datetime import datetime, timezone
+
+from sqlalchemy import select, and_, update
+
 from webapp.database.models.enrolments import Enrolment, Status
 from webapp.database.repositories.generic import GenericRepository
 from webapp.extensions import db
+
 
 class EnrolmentRepository(GenericRepository[Enrolment]):
     def __init__(self) -> None:
@@ -17,4 +21,22 @@ class EnrolmentRepository(GenericRepository[Enrolment]):
 
     def get_active(self) -> list[Enrolment]:
         stmt = select(Enrolment).where(Enrolment.status == Status.ACTIVE)
+        return list(db.session.scalars(stmt).all())
+
+    def mark_expired_enrolments_completed(self) -> list[Enrolment]:
+        update_stmt = (
+            update(Enrolment)
+            .where(
+                Enrolment.course_end_date < datetime.now(timezone.utc),
+                Enrolment.status == Status.ACTIVE
+            )
+            .values(status=Status.COMPLETED)
+        )
+        db.session.execute(update_stmt)
+        db.session.commit()
+
+        stmt = select(Enrolment).where(
+            Enrolment.status == Status.COMPLETED,
+            Enrolment.course_end_date < datetime.now(timezone.utc)
+        )
         return list(db.session.scalars(stmt).all())
