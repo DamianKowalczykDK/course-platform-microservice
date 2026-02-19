9a42a96702f70f452f2a1eb82ae15a7332631338
Damian <damiankowalczyk3688@gmail.com>
2026-02-19 23:35:32 +0100

    test(users-webapp): add test for password regex

--- users/tests/test_users_schema.py
+++ users/tests/test_users_schema.py
@@ -28,6 +28,18 @@ def test_passwords_match() -> None:
     )
     assert user.password == user.password_confirmation
         
-
+def test_passwords_regex_invalid() -> None:
+    with pytest.raises(ValueError, match="Password must have at least 1 uppercase, 1 lowercase, 1 digit, "
+                "1 special char, and be at least 8 characters long") :
+        CreateUserSchema(
+            username="user123",
+            first_name="John",
+            last_name="Doe",
+            email="john@example.com",
+            password="Secret123",
+            password_confirmation="different123.",
+            gender=GenderType.MALE,
+            role="user",
+        )
 
 
